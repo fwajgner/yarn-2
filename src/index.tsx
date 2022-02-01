a13060642b61cf3bed35c06b55937d6447b5ce94
Filip Wajgner <fwajgner@inovatica.com>
2022-02-01 14:12:35 +0100

    Add axe-core/react, remove jsx-a11y plugin

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
